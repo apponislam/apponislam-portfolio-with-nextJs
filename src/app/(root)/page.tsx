758efecf155519ce23663310bb2b935e9442ba19
apponislame <11appon11@gmail.com>
2025-05-20 06:04:58 +0600

    some animation added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -78,22 +78,7 @@ export default function Home() {
                         </Button>
                     </Link>
                 </section>
-                {/* <section id="projects" className="md:container space-y-6 dark:bg-transparent py-10 my-14">
-                    <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-                        <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">{pagesConfig.projects.title}</h2>
-                        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">{pagesConfig.projects.description}</p>
-                    </div>
-                    <div className="mx-auto grid justify-center gap-4  md:w-full lg:grid-cols-3 2xl:w-[1200px]">
-                        {featuredProjects.map((exp) => (
-                            <ProjectCard key={exp._id} project={exp} />
-                        ))}
-                    </div>
-                    <Link href="/projects" className="flex justify-center">
-                        <Button variant={"outline"} className="rounded-xl">
-                            <Icons.chevronDown className="mr-2 h-4 w-4" /> View All
-                        </Button>
-                    </Link>
-                </section> */}
+
                 <FeaturedProjects />
             </div>
         </>
