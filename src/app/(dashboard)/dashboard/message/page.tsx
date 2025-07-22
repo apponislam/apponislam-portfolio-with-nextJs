74a679e870ddd0d7638e680648d2048b2ce36805
apponislame <11appon11@gmail.com>
2025-07-23 02:31:57 +0600

    revalidate added

--- src/app/(dashboard)/dashboard/message/page.tsx
+++ src/app/(dashboard)/dashboard/message/page.tsx
@@ -15,21 +15,34 @@ interface IMessage {
     social?: string;
 }
 
-const fetchMessages = async (): Promise<IMessage[]> => {
+// const fetchMessages = async (): Promise<IMessage[]> => {
+//     try {
+//         const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/messages`, {
+//             next: { revalidate: 2 },
+//         });
+//         console.log(res);
+//         const data = await res.json();
+//         return data.success ? data.data : [];
+//     } catch {
+//         return [];
+//     }
+// };
+
+const Page = async () => {
+    // const messages = await fetchMessages();
+
+    let messages: IMessage[] = [];
+
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/messages`, {
             next: { revalidate: 2 },
         });
-        console.log(res);
         const data = await res.json();
-        return data.success ? data.data : [];
-    } catch {
-        return [];
+        messages = data.success ? data.data : [];
+    } catch (err: any) {
+        console.log(err);
+        messages = [];
     }
-};
-
-const Page = async () => {
-    const messages = await fetchMessages();
 
     return (
         <div>
