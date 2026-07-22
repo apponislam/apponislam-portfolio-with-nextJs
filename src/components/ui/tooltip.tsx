6f17763ee0ed1bed2b1c5bf236790a0f08b8e6aa
Appon Islam <appon2003@gmail.com>
2026-07-22 06:49:09 +0600

    add reusable Tooltip component using Radix UI primitives

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -34,7 +34,7 @@ function TooltipContent({ className, sideOffset = 0, children, ...props }: React
                 {...props}
             >
                 {children}
-                {/* <TooltipPrimitive.Arrow className="bg-primary fill-primary z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" /> */}
+                <TooltipPrimitive.Arrow className="bg-background fill-background border-r border-b border-border z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
             </TooltipPrimitive.Content>
         </TooltipPrimitive.Portal>
     );
